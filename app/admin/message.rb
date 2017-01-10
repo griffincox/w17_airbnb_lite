9ee41c5f3960aa6a27d89125b82c40eee4172656
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:41:29 +0000

    Whitelist columns in Message for ActiveAdmin

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Message do
 
+ permit_params :to_id, :subject, :content, :from_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
