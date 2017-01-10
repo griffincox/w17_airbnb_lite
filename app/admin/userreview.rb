81d59bee17da2cdfe431228a241184e1fcae7fc0
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:41:33 +0000

    Whitelist columns in Userreview for ActiveAdmin

--- app/admin/userreview.rb
+++ app/admin/userreview.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Userreview do
 
+ permit_params :reviewer_id, :reviewed_id, :title, :content, :rating, :booking_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
