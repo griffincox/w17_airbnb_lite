d8b8fac898da1fd1271a692bd11a46459db790de
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:40:55 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "listings#index"
   # Routes for the Listingreview resource:
   # CREATE
   get "/listingreviews/new", :controller => "listingreviews", :action => "new"
