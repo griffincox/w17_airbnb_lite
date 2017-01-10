9738e6255d4f1aa33550c38868265adf5439659c
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:41:01 +0000

    Direct association between listings and bookings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,10 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :availabilities,
+             :class_name => "Booking",
+             :dependent => :destroy
+
   has_many   :userphotos,
              :dependent => :destroy
 
