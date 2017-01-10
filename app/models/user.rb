3cbc343b5c9b6715a050c9a7580043f51ededaa9
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:41:01 +0000

    Direct association between users and bookmarks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :userreviews,
              :foreign_key => "reviewed_id",
              :dependent => :destroy
